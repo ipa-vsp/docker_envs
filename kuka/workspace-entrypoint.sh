88fcada40caf63050c6704aa42875b339381eac5
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2022-10-28 08:57:37 +0200

    change entrypoints

--- kuka/workspace-entrypoint.sh
+++ kuka/workspace-entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 # Build ROS dependency
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
-echo "source ~/colcon_ws/install/setup.bash" >> ~/.bashrc
-echo "export ROS_DOMAIN_ID=69" >> ~/.bashrc
 echo "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >> ~/.bashrc
 source /opt/ros/${ROS_DISTRO}/setup.bash
-export ROS_DOMAIN_ID=69
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-source ~/colcon_ws/install/setup.bash
 
 sudo apt-get update
 rosdep update
