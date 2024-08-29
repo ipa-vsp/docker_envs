aa8220f1e26061fa6997086453e6921be346b634
Vishnuprasad Prachandabhanu <32260301+ipa-vsp@users.noreply.github.com>
2024-08-29 09:33:43 +0200

    Update workspace-entrypoint.sh

--- creator/scripts/workspace-entrypoint.sh
+++ creator/scripts/workspace-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Build ROS dependency
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
+echo "export RCUTILS_COLORIZED_OUTPUT=1" >> ~/.bashrc
 
 sudo apt-get update
 rosdep update
