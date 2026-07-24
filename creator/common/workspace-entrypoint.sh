735aa72ddbbfb1f076c279040e15f66152691136
ipa-vsp <vishnu.pbhat93@gmail.com>
2026-07-24 12:56:20 +0200

    Add Claude Code CLI installation and streamline bashrc configurations

--- creator/common/workspace-entrypoint.sh
+++ creator/common/workspace-entrypoint.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 # Build ROS dependency
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
-echo "export RCUTILS_COLORIZED_OUTPUT=1" >> ~/.bashrc
 
 sudo apt-get update
 rosdep update
 
-# Add parse_git_branch function to .bashrc
-echo 'parse_git_branch() {
-    branch=$(git symbolic-ref --short HEAD 2>/dev/null)
-    remote=$(git config --get branch."$branch".remote 2>/dev/null)
-    if [ -n "$branch" ]; then
-        echo -e "\[\033[0;33m\][$remote: \[\033[0;31m\]$branch\[\033[0;33m\]]"
-    fi
-}' >> ~/.bashrc
-
-# Add PROMPT_COMMAND with parse_git_branch to .bashrc
-echo 'PROMPT_COMMAND='\''PS1="${VIRTUAL_ENV:+(${VIRTUAL_ENV##*/}) }\\[\e[0;32m\\]\\u@\\h:\\[\\e[0;34m\\]\\w$(parse_git_branch)\\[\\e[0m\\]\\$ "'\''' >> ~/.bashrc
+# NOTE: RCUTILS_COLORIZED_OUTPUT, parse_git_branch and the PROMPT_COMMAND are
+# baked into the image bashrc (/etc/bash.bashrc via creator/scripts/bashrc), so
+# they no longer need to be appended here.
 
 # if RMW_IMPLEMENTATION=rmw_zenoh_cpp is set, source the zenoh workspace
 if [ "$RMW_IMPLEMENTATION" = "rmw_zenoh_cpp" ]; then
